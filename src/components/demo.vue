d3320d71c6548fe58a38e808efb850e144240aa1
jituan.han <jituan.han@amh-group.com>
2023-10-08 19:16:46 +0800

    feat: echarts 初始化

--- src/components/demo.vue
+++ src/components/demo.vue
@@ -2,30 +2,63 @@
   <div class="wraper">
     <div class="page-title">迭代质量总结</div>
 
-   <div class="tabs-wraper">
-    <el-tabs v-model='activeName' @tab-click='handleClick'>
-      <el-tab-pane label='横向对比' name='first'>横向对比</el-tab-pane>
-      <el-tab-pane label='当前迭代数据' name='second'>当前迭代数据</el-tab-pane>
-      <el-tab-pane label='线上严重及以上问题列表' name='third'>线上严重及以上问题列表</el-tab-pane>
-      <el-tab-pane label='未关闭缺陷列表' name='fourth'>未关闭缺陷列表</el-tab-pane>
-      <el-tab-pane label=' 典型 bug' name='fifve'> 典型 bug</el-tab-pane>
-    </el-tabs>
-   </div>
-    
+    <div class="tabs-wraper">
+      <el-tabs v-model='activeName' @tab-click='handleClick'>
+        <el-tab-pane label='横向对比' name='first'>
+          <div class="chart" ref="chartRef">
+            echarts
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label='当前迭代数据' name='second'>当前迭代数据</el-tab-pane>
+        <el-tab-pane label='线上严重及以上问题列表' name='third'>线上严重及以上问题列表</el-tab-pane>
+        <el-tab-pane label='未关闭缺陷列表' name='fourth'>未关闭缺陷列表</el-tab-pane>
+        <el-tab-pane label=' 典型 bug' name='fifve'> 典型 bug</el-tab-pane>
+      </el-tabs>
+    </div>
+
   </div>
 </template>
 
 <script>
+import * as echarts from "echarts";
+
 export default {
   name: 'demo-page',
   data() {
     return {
-      activeName: 'second'
+      activeName: 'first'
     }
   },
   mounted() {
+    this.initChart()
   },
   methods: {
+    initChart() {
+      let chartInstance = echarts.getInstanceByDom(this.$refs.chartRef);
+      if (chartInstance == null) {
+        chartInstance = echarts.init(this.$refs.chartRef);
+      }
+
+      const option = {
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: [150, 230, 224, 218, 135, 147, 260],
+            type: 'line'
+          }
+        ]
+      };
+
+      chartInstance.setOption(option)
+
+    },
+
     handleClick(tab, event) {
       console.log(tab, event);
     }
@@ -47,10 +80,15 @@ export default {
   font-size: 18px;
 }
 
-.tabs-wraper{
+.tabs-wraper {
   padding: 8px;
   margin-bottom: 4px;
   background-color: #fff;
   background-color: #fff;
+}
+
+.chart {
+  width: 500px;
+  height: 500px;
 }
 </style>
